7e9a37f5d57424f0cb43aa23e8732f0315aa5da8
Jake Albaugh <jalbaugh@figma.com>
2024-05-22 20:03:17 -0500

    adds text

--- src/ui/Text/Text.tsx
+++ src/ui/Text/Text.tsx
@@ -6,55 +6,38 @@ import {
   type HeadingProps as RACHeadingProps,
   type TextProps as RACTextProps,
 } from "react-aria-components";
-import { Link } from "ui";
+import { Link } from "ui/Link/Link";
 import "./text.css";
 
 export type TextProps = RACTextProps;
 export function Text({ className, ...props }: TextProps) {
-  const classNames = clsx(className, "text");
-  return <RACText elementType="p" {...props} className={classNames} />;
+  const classNames = clsx(className, "text-body-base");
+  return <RACText className={classNames} {...props} />;
 }
-
-export type TextLinkProps = ComponentPropsWithoutRef<typeof Link>;
-export function TextLink({ className, ...props }: TextLinkProps) {
-  const classNames = clsx(className, "text-link");
-  return <Link {...props} className={classNames} />;
+export type TextSmallProps = RACTextProps;
+export function TextSmall({ className, ...props }: TextSmallProps) {
+  const classNames = clsx(className, "text-body-small");
+  return <RACText className={classNames} {...props} />;
 }
-
 export type TextStrongProps = ComponentPropsWithoutRef<"strong">;
 export function TextStrong({ className, ...props }: TextStrongProps) {
-  const classNames = clsx(className, "text-strong");
-  return <strong {...props} className={classNames} />;
+  const classNames = clsx(className, "text-body-strong");
+  return <strong className={classNames} {...props} />;
 }
-
-export type TextCodeProps = ComponentPropsWithoutRef<"code">;
+export type TextEmphasisProps = ComponentPropsWithoutRef<"em">;
+export function TextEmphasis({ className, ...props }: TextEmphasisProps) {
+  const classNames = clsx(className, "text-body-emphasis");
+  return <em className={classNames} {...props} />;
+}
+export type TextLinkProps = ComponentPropsWithoutRef<typeof Link>;
+export function TextLink({ className, ...props }: TextLinkProps) {
+  const classNames = clsx(className, "text-body-link");
+  return <Link className={classNames} {...props} />;
+}
+export type TextCodeProps = RACTextProps;
 export function TextCode({ className, ...props }: TextCodeProps) {
-  const classNames = clsx(className, "text-code");
-  return <code {...props} className={classNames} />;
-}
-
-export type TextSubheadingProps = RACHeadingProps;
-export function TextSubheading({ className, ...props }: TextSubheadingProps) {
-  const classNames = clsx(className, "text-subheading");
-  return <RACHeading className={classNames} {...props} />;
-}
-
-export type TextSubtitleProps = RACHeadingProps;
-export function TextSubtitle({ className, ...props }: TextSubtitleProps) {
-  const classNames = clsx(className, "text-subtitle");
-  return <RACHeading className={classNames} {...props} />;
-}
-
-export type TextHeroProps = RACHeadingProps;
-export function TextHero({ className, ...props }: TextHeroProps) {
-  const classNames = clsx(className, "text-hero");
-  return <RACHeading className={classNames} {...props} />;
-}
-
-export type TextHeadingProps = RACHeadingProps;
-export function TextHeading({ className, ...props }: TextHeadingProps) {
-  const classNames = clsx(className, "text-heading");
-  return <RACHeading className={classNames} {...props} />;
+  const classNames = clsx(className, "text-body-code");
+  return <RACText className={classNames} {...props} />;
 }
 
 export type TextListItemProps = RACTextProps;
@@ -67,4 +50,36 @@ export type TextListProps = ComponentPropsWithoutRef<"ul">;
 export function TextList({ className, ...props }: TextListProps) {
   const classNames = clsx(className, "text-list");
   return <ul className={classNames} {...props} />;
+}
+
+export type TextInputProps = RACTextProps;
+export function TextInput({ className, ...props }: TextInputProps) {
+  const classNames = clsx(className, "text-input");
+  return <RACText className={classNames} {...props} />;
+}
+
+export type TextSubtitleProps = RACHeadingProps;
+export function TextSubtitle({ className, ...props }: TextSubtitleProps) {
+  const classNames = clsx(className, "text-subtitle");
+  return <RACHeading className={classNames} {...props} />;
+}
+export type TextSubheadingProps = RACHeadingProps;
+export function TextSubheading({ className, ...props }: TextSubheadingProps) {
+  const classNames = clsx(className, "text-subheading");
+  return <RACHeading className={classNames} {...props} />;
+}
+export type TextHeadingProps = RACHeadingProps;
+export function TextHeading({ className, ...props }: TextHeadingProps) {
+  const classNames = clsx(className, "text-heading");
+  return <RACHeading className={classNames} {...props} />;
+}
+export type TextTitleHeroProps = RACHeadingProps;
+export function TextTitleHero({ className, ...props }: TextTitleHeroProps) {
+  const classNames = clsx(className, "text-title-hero");
+  return <RACHeading className={classNames} {...props} />;
+}
+export type TextTitlePageProps = RACHeadingProps;
+export function TextTitlePage({ className, ...props }: TextTitlePageProps) {
+  const classNames = clsx(className, "text-title-page");
+  return <RACHeading className={classNames} {...props} />;
 }
