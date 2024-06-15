803e58f3abe1407544d6e2183848e9b4672253db
Jake Albaugh <jalbaugh@figma.com>
2024-06-14 19:21:14 -0500

    adds direction to navigation

--- src/ui/Navigation/Navigation.figma.tsx
+++ src/ui/Navigation/Navigation.figma.tsx
@@ -2,13 +2,22 @@ import { figma } from "@figma/code-connect";
 import { Navigation, NavigationItem } from "./Navigation";
 
 figma.connect(Navigation, "<FIGMA_URL_NAVIGATION>", {
-  props: { children: figma.children("Navigation Item") },
-  example: ({ children }) => <Navigation>{children}</Navigation>,
+  props: {
+    children: figma.children("Navigation Item"),
+    direction: figma.enum("Direction", {
+      Row: "row",
+      Column: "column",
+    }),
+  },
+  example: ({ children, direction }) => (
+    <Navigation direction={direction}>{children}</Navigation>
+  ),
 });
 
 figma.connect(NavigationItem, "<FIGMA_URL_NAVIGATION_ITEM>", {
   props: {
     text: figma.string("Text"),
+
     isSelected: figma.enum("State", {
       On: true,
       Off: undefined,
