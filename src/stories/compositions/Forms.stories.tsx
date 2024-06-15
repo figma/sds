6101770f0539102078f31a1797b09527bdae9377
Jake Albaugh <jalbaugh@figma.com>
2024-06-15 17:54:54 -0500

    forms box and code connect for forms and fields

--- src/stories/compositions/Forms.stories.tsx
+++ src/stories/compositions/Forms.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
 import { FormBox } from "compositions";
-import { Button, ButtonGroup, CheckboxField, Input, InputField } from "ui";
+import { Button, ButtonGroup, CheckboxField, InputField } from "ui";
 
 const meta: Meta<typeof FormBox> = {
   component: FormBox,
@@ -12,15 +12,9 @@ export default meta;
 export const Default: StoryObj<typeof FormBox> = {
   render: (args) => (
     <FormBox {...args} onSubmit={() => {}}>
-      <InputField label="Email">
-        <Input placeholder="you@example.com" />
-      </InputField>
-      <InputField label="Password">
-        <Input value="••••••" placeholder="Placeholder here..." />
-      </InputField>
-      <CheckboxField>
-        <CheckboxField defaultSelected />
-      </CheckboxField>
+      <InputField label="Email" />
+      <InputField label="Password" />
+      <CheckboxField label="Label" description="Description" />
       <ButtonGroup align="justify">
         <Button onPress={() => {}} variant="primary">
           Register
