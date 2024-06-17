d1787b0dc1671edac6804a2a6e4875eabfe07fea
Jake Albaugh <jalbaugh@figma.com>
2024-06-17 10:52:12 -0500

    initial dev resources

--- scripts/dev-resources/app.mjs
+++ scripts/dev-resources/app.mjs
@@ -1,5 +1,10 @@
 import * as figmaJSON from "../../figma.config.json" with { type: "json" };
 
+// run with node --env-file=.env app.mjs
+const TOKEN = process.env.FIGMA_ACCESS_TOKEN;
+const URL_BASE = "https://api.figma.com/v1/files";
+const FILE_KEY = "J0KLPKXiONDRssXD1AX9Oi";
+
 const urls = figmaJSON.default.codeConnect.documentUrlSubstitutions;
 
 const resourceMap = {
@@ -7,5 +12,44 @@ const resourceMap = {
   "<FIGMA_URL_ACCORDION_ITEM>": ["https://figma.github.io/sds/storybook"],
 };
 
+run();
+
+async function run() {
+  const devResources = await getDevResources();
+  console.log(devResources);
+  // await Promise.all(devResources.map(({ id }) => deleteDevResource(id)));
+  console.log("DONE!");
+}
+
+async function deleteDevResource(id) {
+  try {
+    const fileResponse = await fetch(
+      `${URL_BASE}/${FILE_KEY}/dev_resources/${id}`,
+      {
+        method: "DELETE",
+        headers: { "X-FIGMA-TOKEN": TOKEN },
+      },
+    );
+    return await fileResponse.json();
+  } catch (e) {
+    throw e;
+  }
+}
+async function getDevResources() {
+  try {
+    const fileResponse = await fetch(`${URL_BASE}/${FILE_KEY}/dev_resources`, {
+      method: "GET",
+      headers: { "X-FIGMA-TOKEN": TOKEN },
+    });
+    const data = await fileResponse.json();
+    return await fileRESTResponseToJSON(data);
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function fileRESTResponseToJSON(data) {
+  return data.dev_resources;
+}
 // TODO: get all node ids from code connect json,
 // TODO: make dev resources request to update / remove for all nodes
