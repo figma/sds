90307448c1772b7cb795a616f37bbbc8e131bf00
Jake Albaugh <jalbaugh@figma.com>
2024-06-17 10:22:16 -0500

    improved footer

--- src/compositions/Footers/Footers.tsx
+++ src/compositions/Footers/Footers.tsx
@@ -13,7 +13,7 @@ import {
 
 export type FooterProps = Omit<SectionProps, "variant" | "padding" | "src">;
 export function Footer({ className, ...props }: FooterProps) {
-  const { isMobile, isTablet, isTabletDown } = useMediaQuery();
+  const { isTabletDown } = useMediaQuery();
   const listDensity = isTabletDown ? "tight" : "default";
   return (
     <Section
@@ -25,107 +25,114 @@ export function Footer({ className, ...props }: FooterProps) {
       {...props}
     >
       <Flex wrap type="quarter" gap="600" container>
-        <FlexItem size={isTablet ? "full" : "minor"}>
-          <Flex
-            direction={isTabletDown ? "row" : "column"}
-            alignPrimary="space-between"
-            gap="600"
-            alignSecondary="start"
-          >
+        <FlexItem size="minor">
+          <Flex direction="column" gap="600" alignSecondary="start">
             <FlexItem>
               <Logo />
             </FlexItem>
-            <SocialButtons />
-          </Flex>
-        </FlexItem>
-        <FlexItem size={isTablet ? "full" : "major"}>
-          <Flex
-            type="auto"
-            gap="600"
-            wrap
-            alignPrimary="stretch"
-            direction={isMobile ? "column" : "row"}
-          >
-            <TextLinkList
-              density={listDensity}
-              title={<TextStrong>Use cases</TextStrong>}
-            >
+            <TextLinkList density={listDensity}>
               <TextListItem>
-                <TextLink href="#">UI design</TextLink>
+                <TextLink href="https://www.figma.com">figma.com</TextLink>
               </TextListItem>
               <TextListItem>
-                <TextLink href="#">UX design</TextLink>
+                <TextLink href="https://www.x.com/figma">X</TextLink>
               </TextListItem>
               <TextListItem>
-                <TextLink href="#">Wireframing</TextLink>
+                <TextLink href="https://instagram.com/figma">
+                  Instagram
+                </TextLink>
               </TextListItem>
               <TextListItem>
-                <TextLink href="#">Diagramming</TextLink>
+                <TextLink href="https://www.youtube.com/@Figma">
+                  YouTube
+                </TextLink>
               </TextListItem>
               <TextListItem>
-                <TextLink href="#">Brainstorming</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">Online whiteboard</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">Team collaboration</TextLink>
-              </TextListItem>
-            </TextLinkList>
-            <TextLinkList
-              density={listDensity}
-              title={<TextStrong>Explore</TextStrong>}
-            >
-              <TextListItem>
-                <TextLink href="#">Design</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">Prototyping</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">Development features</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">Design systems</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">Collaboration features</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">Design process</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">FigJam</TextLink>
-              </TextListItem>
-            </TextLinkList>
-            <TextLinkList
-              density={listDensity}
-              title={<TextStrong>Resources</TextStrong>}
-            >
-              <TextListItem>
-                <TextLink href="#">Blog</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">Best practices</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">Colors</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">Color wheel</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">Support</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">Developers</TextLink>
-              </TextListItem>
-              <TextListItem>
-                <TextLink href="#">Resource library</TextLink>
+                <TextLink href="https://www.linkedin.com/company/figma/">
+                  LinkedIn
+                </TextLink>
               </TextListItem>
             </TextLinkList>
           </Flex>
         </FlexItem>
+        <TextLinkList
+          density={listDensity}
+          title={<TextStrong>Use cases</TextStrong>}
+        >
+          <TextListItem>
+            <TextLink href="#">UI design</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">UX design</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Wireframing</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Diagramming</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Brainstorming</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Online whiteboard</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Team collaboration</TextLink>
+          </TextListItem>
+        </TextLinkList>
+        <TextLinkList
+          density={listDensity}
+          title={<TextStrong>Explore</TextStrong>}
+        >
+          <TextListItem>
+            <TextLink href="#">Design</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Prototyping</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Development features</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Design systems</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Collaboration features</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Design process</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">FigJam</TextLink>
+          </TextListItem>
+        </TextLinkList>
+        <TextLinkList
+          density={listDensity}
+          title={<TextStrong>Resources</TextStrong>}
+        >
+          <TextListItem>
+            <TextLink href="#">Blog</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Best practices</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Colors</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Color wheel</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Support</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Developers</TextLink>
+          </TextListItem>
+          <TextListItem>
+            <TextLink href="#">Resource library</TextLink>
+          </TextListItem>
+        </TextLinkList>
       </Flex>
     </Section>
   );
