08e3961b8cc08b7400873d9e8a449dc7619602b0
Jake Albaugh <jalbaugh@figma.com>
2024-07-01 14:25:16 -0500

    icons script cleanup and rerun

--- src/ui/icons/IconMinus.tsx
+++ src/ui/icons/IconMinus.tsx
@@ -1,4 +1,4 @@
 import { IconProps, Icon } from "primitives";
 export const IconMinus = (props: IconProps) => (
-  <Icon {...props}><path d="M3.33337 7.2C2.89155 7.2 2.53337 7.55817 2.53337 8C2.53337 8.44183 2.89155 8.8 3.33337 8.8V7.2ZM12.6667 8.8C13.1085 8.8 13.4667 8.44183 13.4667 8C13.4667 7.55817 13.1085 7.2 12.6667 7.2V8.8ZM3.33337 8.8H12.6667V7.2H3.33337V8.8Z" fill="var(--svg-fill-color)"/></Icon>
+  <Icon {...props}><path d="M3.33337 8H12.6667" stroke="var(--svg-stroke-color)" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"/></Icon>
 );
