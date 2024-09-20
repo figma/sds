c3ae31a6da46d502ebb6ec8f3b486004ca0bea49
Tristan Lee <uklee@tangunsoft.com>
2024-09-20 14:54:48 +0900

    Update Heroes.tsx

--- src/ui/compositions/Sections/Heroes.tsx
+++ src/ui/compositions/Sections/Heroes.tsx
@@ -4,7 +4,7 @@ export type HeroProps = Omit<SectionProps, "padding" | "paddingY" | "paddingX">;
 export function Hero({ children, ...props }: HeroProps) {
   return (
     <Section padding="1600">
-      <Flex container alignPrimary="center" gap="600" {...props}>
+      <Flex container alignPrimary="center" alignSecondary="center" gap="600" direction="column" {...props}>
         {children}
       </Flex>
     </Section>
